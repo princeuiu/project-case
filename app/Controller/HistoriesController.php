bd426719a5a99a89af3f610af887cc2e80de16cf
shovan <exorcist.shovan@gmail.com>
2015-07-01 14:32:12 +0600

    history module

--- app/Controller/HistoriesController.php
+++ app/Controller/HistoriesController.php
@@ -3,64 +3,61 @@
 App::uses('AppController', 'Controller');
 
 
-class CategoriesController extends AppController {
+class HistoriesController extends AppController {
 
-    public $name = 'Categories';
-    
-    public $theme = 'InflackPos';
+    public $name = 'Histories';
+
+    public $uses = array('History', 'Lawsuit');
 
     public function admin_add(){
         if(!empty($this->data)){
-            $upImage = $this->data['Category']['image'];
-            $upDir = WWW_ROOT . 'img' . DS . 'categories' . DS . 'original' . DS;
-            $upImgData = array('file' => $upImage, 'path' => $upDir);
-            $upImgName = $this->Qimage->copy($upImgData);
-            if($upImgName){
-                $catData = $this->data;
-                unset($catData['Category']['image']);
-                $catData['Category']['image'] = $upImgName;
-                $resizeDir = WWW_ROOT . 'img' . DS . 'categories' . DS . 'resize' . DS;
-                $thumbDir = WWW_ROOT . 'img' . DS . 'categories' . DS . 'thumb' . DS;
-                $resizeImgData = array(
-                    'file' => $upDir . $upImgName,
-                    'width' => '400',
-                    'output' => $resizeDir
-                    );
-                $thumbImgData = array(
-                    'file' => $upDir . $upImgName,
-                    'width' => '85',
-                    'output' => $thumbDir
-                    );
-                if($this->Qimage->resize($resizeImgData)){
-                    if($this->Qimage->resize($thumbImgData)){
-                        if($this->Category->save($catData)){
-                            $this->Session->setFlash('<div class="alert alert-success">' . __('Category added successfully.') . '</div>');
-                            return $this->redirect(array('controller' => 'categories', 'action' => 'edit', $this->Category->id));
-                        }
-                        else{
-                            $this->Session->setFlash('<div class="alert alert-danger">' . __('Can\'t save Category now, Please try again later.') . '</div>');
-                            return;
-                        }
-                    }
-                }
+            $lawsuit_id = $this->data['History']['lawsuit_id'];
+            $title = $this->data['History']['title'];
+            $description = $this->data['History']['description'];
+            $reporting_date = $this->data['History']['reporting_date'];
+            $remark = $this->data['History']['remark'];
+
+            if($this->History->save($this->data)){
+                $this->Session->setFlash('<div class="alert alert-success"><button type="button" class="close" data-dismiss="alert">×</button>' . __('History added successfully.') . '</div>');
+                return $this->redirect(array('controller' => 'histories', 'action' => 'edit', $this->History->id));
             }
-            $imgUpErr = $this->Qimage->getErrors();
-            if(!empty($imgUpErr)){
-                $error = implode(',', $imgUpErr);
-                $this->Session->setFlash('<div class="alert alert-danger">' . __('Can\'t save Category now, Please try again later.' . $error) . '</div>');
+            else{
+                $this->Session->setFlash('<div class="alert alert-danger"><button type="button" class="close" data-dismiss="alert">×</button>' . __('Can\'t save History now, Please try again later.') . '</div>');
+                return;
             }
+//                if($this->Qimage->resize($resizeImgData)){
+//                    if($this->Qimage->resize($thumbImgData)){
+//                        if($this->Category->save($catData)){
+//                            $this->Session->setFlash('<div class="alert alert-success">' . __('Category added successfully.') . '</div>');
+//                            return $this->redirect(array('controller' => 'categories', 'action' => 'edit', $this->Category->id));
+//                        }
+//                        else{
+//                            $this->Session->setFlash('<div class="alert alert-danger">' . __('Can\'t save Category now, Please try again later.') . '</div>');
+//                            return;
+//                        }
+//                    }
+//                }
+//            }
+//            $imgUpErr = $this->Qimage->getErrors();
+//            if(!empty($imgUpErr)){
+//                $error = implode(',', $imgUpErr);
+//                $this->Session->setFlash('<div class="alert alert-danger">' . __('Can\'t save Category now, Please try again later.' . $error) . '</div>');
+//            }
         }
         $options = array(
             'NOT' => array(
-                'parent_id' => null, 
+                'parent_id' => null,
             ),
         );
-        $parents = $this->Category->generateTreeList(null,null,null," - ");
-        //pr($parents);
-        
-        $this->set(compact('parents'));
+        $lawsuits = $this->Lawsuit->find('list', array(
+            'fields' => array('Lawsuit.id', 'Lawsuit.number'),
+            'conditions' => array('Lawsuit.status'=>'active')
+        ));
+//        pr($lawsuits);
+
+        $this->set(compact('lawsuits'));
     }
-    
+
     public function admin_edit($id) {
         if($id == null){
             throw new BadRequestException();
@@ -82,12 +79,12 @@ class CategoriesController extends AppController {
                         'file' => $upDir . $upImgName,
                         'width' => '400',
                         'output' => $resizeDir
-                        );
+                    );
                     $thumbImgData = array(
                         'file' => $upDir . $upImgName,
                         'width' => '85',
                         'output' => $thumbDir
-                        );
+                    );
                     if($this->Qimage->resize($resizeImgData)){
                         if($this->Qimage->resize($thumbImgData)){
                             if($this->Category->save($catData)){
@@ -122,62 +119,62 @@ class CategoriesController extends AppController {
         }
 
         $this->data = $this->Category->read();
-        
+
         $options = array(
             'NOT' => array(
-                'parent_id' => 0, 
+                'parent_id' => 0,
             ),
         );
         $parents = $this->Category->generateTreeList(null,null,null," - ");
         //pr($parents);
-        
+
         $this->set(compact('parents'));
 
-        
+
         $this->render('admin_add');
     }
-    
+
     public function admin_index() {
         extract($this->params["named"]);
         $options = array(
             'NOT' => array(
-                'parent_id' => 0, 
+                'parent_id' => 0,
             ),
         );
         if(isset($search)){
             $options["Category.title like"]="%$search%";
         }
         else $search="";
-        
+
         $this->paginate["Category"]["order"]="Category.created DESC";
-        
+
         $categories = $this->paginate('Category', $options);
         //pr($categories);
         $this->set(compact('categories','search'));
-        
-        
+
+
         //$this->set("search",$search);
     }
-    
+
     public function index(){
         extract($this->params["named"]);
-        
+
         if(isset($search)){
             $options["Category.title like"]="%$search%";
         }
         else $search="";
-        
+
         $this->paginate["Category"]["order"]="Category.created DESC";
-        
+
         $categories = $this->paginate('Category', $options);
         $count = count($categories);
         $itemEachRow = $count / 3;
         //pr($categories);
         $this->set(compact('categories','search', 'itemEachRow'));
     }
-    
-    
-    
+
+
+
     function admin_remove_image($name) {
         $this->Category->updateAll(array("image"=>"''"),array("image"=>"$name"));
         @unlink(WWW_ROOT."img/categories/original/".$name);
---
+++ app/View/Themed/Lawfirm/Histories/admin_add.ctp
@@ -0,0 +1,80 @@
+<div class="row-fluid sortable">
+    <div class="box span12">
+        <div class="box-header" data-original-title>
+            <h2><i class="halflings-icon white edit"></i><span class="break"></span><?php echo __('Add Case History'); ?></h2>
+        </div>
+        <div class="box-content">
+            <?php
+            echo $this->Form->create('History', array(
+                'action' => $this->action,
+                'class' => 'form-horizontal',
+                'inputDefaults' => array(
+                    'div' => false,
+                    'label' => false
+                )
+            ));
+            ?>
+            <fieldset>`
+                <div class="control-group">
+                    <label class="control-label" for="CaseNumber">Case Number</label>
+                    <div class="controls">
+                        <?php
+                        echo $this->Form->input('lawsuit_id', array('class' => 'span6 typeahead', 'placeholder' => 'Case Number', 'options' => $lawsuits, 'error' => array(
+                            'attributes' => array('escape' => false)
+                        )));
+                        ?>
+                    </div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label" for="Title">Title </label>
+                    <div class="controls">
+                        <?php
+                        echo $this->Form->input('title', array('class' => 'span6 typeahead', 'placeholder' => 'Title', 'error' => array(
+                            'attributes' => array('escape' => false)
+                        )));
+                        ?>
+                    </div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label" for="Description">Description </label>
+                    <div class="controls">
+                        <?php
+                        echo $this->Form->input('description', array('type'=>'textarea', 'class' => 'span6 typeahead', 'placeholder' => 'Description', 'error' => array(
+                            'attributes' => array('escape' => false)
+                        )));
+                        ?>
+                    </div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label" for="ReportingDate">Reporting Date </label>
+                    <div class="controls">
+                        <?php
+                        echo $this->Form->input('reporting_date', array('type'=>'date', 'class' => 'input-xlarge datepicker hasDatepicker', 'placeholder' => 'Reporting Date', 'error' => array(
+                            'attributes' => array('escape' => false)
+                        )));
+                        ?>
+                    </div>
+                </div>
+                <div class="control-group">
+                    <label class="control-label" for="Remarks">Remarks </label>
+                    <div class="controls">
+                        <?php
+                        echo $this->Form->input('remark', array('type'=>'textarea', 'class' => 'span6 typeahead', 'placeholder' => 'Remarks', 'error' => array(
+                            'attributes' => array('escape' => false)
+                        )));
+                        ?>
+                    </div>
+                </div>
+
+                <div class="form-actions">
+                    <button type="submit" class="btn btn-primary">Save changes</button>
+                    <button type="reset" class="btn">Cancel</button>
+                </div>
+            </fieldset>
+            </form>
+
+        </div>
+    </div><!--/span-->
+
+</div><!--/row-->
+
