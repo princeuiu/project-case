777b139229c3195f2645daa40c21af6785d96269
shovansarker <exorcist.shovan@gmail.com>
2015-08-08 17:29:35 +0600

    1. dashboard updated

--- app/Controller/DashboardController.php
+++ app/Controller/DashboardController.php
@@ -6,7 +6,7 @@ App::uses('AppController', 'Controller');
 class DashboardController extends AppController {
 
     public $name = 'Dashboard';
-    public $uses = array('History', 'Lawsuit', 'Client', 'Activity');
+    public $uses = array('History', 'Lawsuit', 'Client', 'Activity', 'Task');
 
     public function beforeFilter() {
         parent::beforeFilter();
@@ -18,8 +18,31 @@ class DashboardController extends AppController {
             'conditions' => array('Activity.user_id' => $logged_in_user_id),
             'order' => array('Activity.created DESC')
         ));
-        print_r($activity_data);die;
+
+        $assigned_tasks = $this->Task->find('all',array(
+            'conditions' => array('Task.assigned_to' => $logged_in_user_id),
+            'order' => array('Task.dead_line ASC')
+        ));
+
+        $new_lawsuits = $this->Lawsuit->find('all', array(
+            'conditions' => array('Lawsuit.number' => ''),
+            'order' => array('Lawsuit.created ASC')
+        ));
+        $dateTo = date('Y-m-d H:i:s',mktime(0, 0, 0, date("m"), date("d")+3,   date("Y")));
+        $dateFrom = date('Y-m-d H:i:s',mktime(0, 0, 0, date("m"), date("d"),   date("Y")));
+        //$today =now();
+//        echo $dateFrom; die;
+        $new_histories = $this->History->find('all',array(
+            'conditions' => array('History.reporting_date >= ' => $dateFrom, 'History.reporting_date < ' => $dateTo),
+            'order' => array('History.reporting_date ASC')
+        ));
+
+
+
+//        print_r($new_histories);die;
+        $this->set(compact('assigned_tasks', 'new_lawsuits', 'new_histories'));
     }
+
 
 
 
